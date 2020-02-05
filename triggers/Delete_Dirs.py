5a0874357b3745dacd82681a6bd64ad498e483ca
Steven Johnson <steven.j2019@pm.me>
2020-02-05 19:58:30 +0000

    Create Delete_Dirs.py

---
+++ triggers/Delete_Dirs.py
@@ -0,0 +1,8 @@
+function Delete_Dirs(Directory):
+    for arg in args:
+        subprocess.call("sudo rm -rf"+Directory, shell = True)
+        
+with open("Delete_Dirs.csv") as Input:
+    csv_reader = csv_reader(Input, delimiter=",")
+    for row in Input:
+        Delete_Dirs(row)
