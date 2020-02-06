6c2a47bfcfa24593da8c8a501ab27c7e71418cdf
Steven Johnson <steven.j2019@pm.me>
2020-02-06 12:15:14 +0000

    Add files via upload
    added:
    some graphical changes to make app more symmetric 
    added details such as text on buttons 
    working drop down functionality for triggers based on context of trigger directory (adds user custom-ability to the platform) 
    still missing: 
    basic functionality still not implemented 
    dynamically finding devices based on shell output still not implemented 
    known issues: 
    when launching from file location cannot find the Qt Module to allow UI elements to render

--- app/Application.py
+++ app/Application.py
@@ -1,32 +1,21 @@
 #BusKIllUI Class
-
+import os
 import sys
+
 from sys import platform
 
 from functools import partial
+import fnmatch
 
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtWidgets import QGridLayout
-from PyQt5.QtWidgets import QLineEdit
-from PyQt5.QtWidgets import QPushButton
-from PyQt5.QtWidgets import QVBoxLayout
-from PyQt5.QtWidgets import QComboBox
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QLineEdit, QPushButton, QVBoxLayout, QComboBox
 
-#helper functions
-def get_sys():
-    if platform.lower() == "darwin":
-        return " Mac"
-    else:
-        return " Linux"
 class BusKillUI(QMainWindow):
 
     def __init__(self):
         super().__init__()
 
-        Platform = get_sys()
+        Platform = BusKillCtrl.get_sys()
         self.setWindowTitle("BusKill " + Platform)
 
         #layout
@@ -41,32 +30,64 @@ class BusKillUI(QMainWindow):
 
         if Platform == " Mac":
             self._ConfigureApplicationButton()
-        else:
+        elif Platform == "": #whatever Unix Os's Return
             self._RunSetupUtilityButton()
-
+        else:
+            print("an error has occured") # create a popup for errors to be universally utilised
     def _DropDownDriveSelection(self):
         self.DeviceDropDown = QComboBox()
         self.DeviceDropDown.addItem("--Device--")
-        Devices = {} #populates options
+        Devices = [] #populates options
+        #for Device in Devices:
+        #    self.DeviceDropDown.addItem(Device)
         self.DeviceDropDown.setFixedHeight(35)
         self.generalLayout.addWidget(self.DeviceDropDown)
 
     def _DropDownTriggerSelection(self):
         self.TriggerDropDown = QComboBox()
-        Triggers = {} #populates available triggers
+        self.TriggerDropDown.addItem("--Trigger--")
+        Triggers = BusKillCtrl.Get_Triggers() #populates available triggers
+        for trigger in Triggers:
+            raw = trigger.split(".")
+            trigger_name = raw[0]
+            self.TriggerDropDown.addItem(trigger.split(".")[0])
         self.TriggerDropDown.setFixedHeight(35)
         self.generalLayout.addWidget(self.TriggerDropDown)
-        
+
 
     def _RunSetupUtilityButton(self):
-        self.SetupButton = QPushButton()
+        self.SetupButton = QPushButton("Run Setup")
         self.SetupButton.setFixedSize(120, 40)
         self.generalLayout.addWidget(self.SetupButton)
 
     def _ConfigureApplicationButton(self):
-        self.ApplicationButton = QPushButton()
-        self.ApplicationButton.setFixedSize(120,40)
+        self.ApplicationButton = QPushButton("Run Agent")
+        self.ApplicationButton.setFixedSize(200,40)
         self.generalLayout.addWidget(self.ApplicationButton)
+
+class BusKillCtrl:
+
+    def __init__(self, view):
+        self._view = view
+        self._connectSignals()
+
+    def Get_Triggers():
+        Triggers = fnmatch.filter(os.listdir('../triggers/'), '*.py')
+        return Triggers
+
+    def Get_Devices():
+        Devices = ["1","2"]
+        return Devices
+
+    def get_sys():
+        if platform.lower() == "darwin":
+            return " Mac"
+        else:
+            return " Linux"
+
+    #def execute_trigger(): # MacOSX Only!!!!
+
+    #def Run_Setup(): # Linux Only
 
 def main():
     BusKill = QApplication(sys.argv)
